c2172349e417e9ad7647d62d7e082db9964aa783
abby-albert <abigailalbert2007@gmail.com>
2024-04-23 12:19:36 -0700

    highscore

--- model/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# Create a SQLAlchemy instance
-db = SQLAlchemy()
-
-# Import the HighScore model so it can be accessed from other parts of the application
-from .highscores import HighScore
