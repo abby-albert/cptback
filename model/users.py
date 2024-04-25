4ff3480d3233e99d779adb540e6f0cf4d926941c
abby-albert <abigailalbert2007@gmail.com>
2024-04-25 14:14:37 -0700

    signup

--- model/users.py
+++ model/users.py
@@ -8,6 +8,41 @@ from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+@app.route('/api/users', methods=['POST'])
+def signup():
+    # Get data from request
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    # Check if username already exists
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return jsonify({'message': 'Username already exists'}), 400
+
+    # Create new user
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
 ''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
 
