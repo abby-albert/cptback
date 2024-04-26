715eecfa16f7f3f3585e9ac3b101499c9830b144
abby-albert <abigailalbert2007@gmail.com>
2024-04-25 22:37:40 -0700

    signup

--- model/users.py
+++ model/users.py
@@ -19,6 +19,30 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
+def initUsers():
+    # Sample user data
+    users_data = [
+        {'name': 'Scottie Sheffler', 'uid': 'scottie', 'pasword': '123scottie'},
+        {'name': 'Nelly Korda', 'uid': 'nelly', 'password': '123nelly'},
+        {'name': 'Tiger Woods', 'uid': 'tiger', 'password': '123tiger'},
+        {'name': 'Justin Thomas', 'uid': 'jt', 'password': '123jt'} 
+       ]
+
+    # Add sample users to the database
+    for user_data in users_data:
+        name = user_data['name']
+        uid = user_data['uid']
+        password = user_data['password']
+
+        # Check if username already exists
+        existing_user = User.query.filter_by(uid=uid).first()
+        if not existing_user:
+            # Create new user
+            new_user = User(name=name, uid=uid, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+
+# API route for user signup
 @app.route('/api/users', methods=['POST'])
 def signup():
     # Get data from request
@@ -39,4 +63,5 @@ def signup():
     return jsonify({'message': 'User created successfully'}), 201
 
 if __name__ == '__main__':
+    # Run the Flask app
     app.run(debug=True)
