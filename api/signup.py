c922fcbe414d53c10383d598c6446e2becee122c
abby-albert <abigailalbert2007@gmail.com>
2024-04-29 14:20:15 -0700

    signup

--- api/signup.py
+++ api/signup.py
@@ -1,40 +1,61 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash
+import json, jwt
+from flask import Blueprint, request, jsonify, current_app, Response
+from flask_restful import Api, Resource 
+from datetime import datetime
+from auth_middleware import token_required
+from model.signups import User
+signup_api = Blueprint('signup_api', __name__,
+                   url_prefix='/api/signup')
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
-db = SQLAlchemy(app)
-api = Api(app)
+api = Api(signup_api)
+class SignupAPI:
+    class _CRUD(Resource):  
+        def post(self): 
+            ''' Read data for json body '''
+            body = request.get_json()
+            ''' Avoid garbage in, error checking '''
+        
+            Username = body.get('Username')
+            if Username is None or len(Username) < 2:
+                return {'message': f'Username is missing, or is less than 2 characters'}, 400
+          
+            Password = body.get('Password')
+            if Password is None or Password < 0 :
+                return {'message': f'Password has to be positive number'}, 400
+            ''' #1: Key code block, setup USER OBJECT '''
+            newUser = User(Username=Username,
+                      Password=Password)
+            ''' #2: Key Code block to add user to database '''
+            just_added_user = newUser.create()
+            if just_added_user:
+                return jsonify(just_added_user.read())
+         
+            return {'message': f'Processed {Username}, either a format error or it is duplicate'}, 400
+        def get(self):
+            users = users.query.all()    
+            json_ready = [user.read() for user in users]  
+            return jsonify(json_ready)  
+        def delete(self):  
+            ''' Find user by ID '''
+            body = request.get_json()
+            del_user = body.get('Username')
+            result = User.query.filter(User._Active == del_user).first()
+            if result is None:
+                 return {'message': f'pulse {del_user} not found'}, 404
+            else:
+                result.delete()
+                print("delete")
+    class _get(Resource):
+        def get(self, lname=None):  
+            print(lname)
+            if lname:
+                result = User.query.filter_by(_Userame=lname).first()
+                print(result)
+                if result:
+                    print(result)
+                    return jsonify([result.read()])  # Assuming you have a read() method in your PulseyApi model
+                else:
+                    return jsonify({"message": "User not found"}), 404
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    uid = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-
-class UserSignup(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('uid', type=str, required=True, help='uid is required')
-        parser.add_argument('password', type=str, required=True, help='Password is required')
-        args = parser.parse_args()
-
-        uid = args['uid']
-        password = args['password']
-
-        if User.query.filter_by(uid=uid).first():
-            return {'message': 'uid already exists'}, 409
-
-        hashed_password = generate_password_hash(password)
-        new_user = User(uid=uid, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return {'message': 'User created successfully'}, 201
-
-api.add_resource(UserSignup, '/signup')
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+    api.add_resource(_CRUD, '/')
+    api.add_resource(_get, '/<string:lname>')
--- model/signups.py
+++ model/signups.py
@@ -1,35 +1,91 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
-db = SQLAlchemy(app)
-
+""" database dependencies to support sqliteDB examples """
+from random import randrange
+from datetime import date
+import os, base64
+import json
+from __init__ import app, db
+from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
+''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    uid = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    __tablename__ = 'user'  
+    _Active = db.Column(db.String(255), primary_key=True)
+    _Exercise = db.Column(db.Integer, unique=False, nullable=False)
+    
+    def __init__(self, Username, Password):
+        self._Active = Username   
+        self._Exercise = Password
+        
+    @property
+    def Username(self):
+        return self._Username
+    
+    @Username.setter
+    def Username(self, Username):
+        self._Active = Username
+    
+    @property
+    def Password(self):
+        return self._Password
+   
+    @Password.setter
+    def Password(self, Password):
+        self._Password = Password
+   
+    def __str__(self):
+        return json.dumps(self.read())
+    
+    def create(self):
+        try:
+            db.session.add(self)  
+            db.session.commit()  
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
 
-@app.route('/signup', methods=['POST'])
-def signup():
-    data = request.get_json()
-    uid = data.get('uid')
-    password = data.get('password')
+    def read(self):
+        return {
+            "Username": self.Username,
+            "Password": self.Password
+        }
 
-    if not uid or not password:
-        return jsonify({'message': 'uid and password are required'}), 400
+    def update(self, Username="", Password=0):
+        """only updates values with length"""
+        if len(Username) > 0:
+            self.Username = Username
+        if Password >= 0:
+            self.Password = Password
+        db.session.commit()
+        return self
+  
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return None
+"""Database Creation and Testing """
 
-    if User.query.filter_by(uid=uid).first():
-        return jsonify({'message': 'uid already exists'}), 409
-
-    hashed_password = generate_password_hash(password)
-    new_user = User(uid=uid, password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'message': 'User created successfully'}), 201
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+def initUsers():
+    with app.app_context():
+        """Create database and tables"""
+        db.create_all()
+        """Tester data for table"""
+       
+        Users = []
+        try:
+            with open(r'users.json','r') as json_file:
+                data = json.load(json_file)
+        except Exception as error:
+            print("failed")
+        for item in data:
+            # print(item)
+            p_toadd = Users(Username=item['Username'], Password=item['Password'])
+            Users.append(p_toadd)
+        """Builds sample user/note(s) data"""
+        for p in Users:
+            try:
+                p.create()
+            except IntegrityError:
+                '''fails with bad or duplicate data'''
+                db.session.remove()
+                print(f"Records exist, duplicate email, or error: {p.Users}")
