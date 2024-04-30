0df03485dd05c09ea06d0af82526a9052b246470
abby-albert <abigailalbert2007@gmail.com>
2024-04-29 17:43:10 -0700

    signup

--- api/signup.py
+++ api/signup.py
@@ -6,12 +6,12 @@ from auth_middleware import token_required
 
 from model.signups import Username, Password
 
-Signup_api = Blueprint('Signup_api', __name__,
-                   url_prefix='/api/Signup')
+signup_api = Blueprint('signup_api', __name__,
+                   url_prefix='/api/signup')
 
-api = Api(Signup_api)
+api = Api(signup_api)
 
-class SignupAPI:        
+class signupAPI:        
     class _Log(Resource):
         @token_required
         def post(self, current_user): # Log food intake or exercise activity
@@ -21,39 +21,39 @@ class SignupAPI:
             if log_type == 'Username':
                 Username = body.get('Username')
                 if not Username:
-                    return {'message': 'Username missing'}, 400
+                    return {'message': 'username is missing'}, 400
 
                 new_Username = Username(Username=Username, user_id=current_user.id)
-                created_User = new_Username.create()
+                created_Username = new_Username.create()
 
-                if created_User:
-                    return jsonify(created_User.serialize()), 201
+                if created_Username:
+                    return jsonify(created_Username.serialize()), 201
                 else:
-                    return {'message': 'Failed to create user'}, 500
+                    return {'message': 'Failed to signin'}, 500
 
             elif log_type == 'Password':
                 Password = body.get('Password')
                 if not Password:
-                    return {'message': 'Password is missing'}, 400
+                    return {'message': 'Password missing'}, 400
 
-                Password = Password(Password=Password, user_id=current_user.id)
-                created_User = Password.create()
+                new_Password = Password(Password=Password, user_id=current_user.id)
+                created_Password = new_Password.create()
 
-                if created_User:
-                    return jsonify(created_User.serialize()), 201
+                if created_Password:
+                    return jsonify(created_Password.serialize()), 201
                 else:
-                    return {'message': 'Failed to Sign Up'}, 500
+                    return {'message': 'Failed to sign up'}, 500
 
             else:
-                return {'message': 'Invalid information'}, 400
+                return {'message': 'Invalid user type'}, 400
 
         @token_required
         def get(self, current_user):  # Retrieve user's fitness log
-            Usernames = Usernames.query.filter_by(user_id=current_user.id).all()
-            Passwords = Passwords.query.filter_by(user_id=current_user.id).all()
+            Username = Username.query.filter_by(user_id=current_user.id).all()
+            Password = Password.query.filter_by(user_id=current_user.id).all()
 
-            Username_logs = [Username_logs.serialize() for Username_logs in Username]
-            Password_logs = [Password_logs.serialize() for Password_logs in Password]
+            Username_logs = [Username.serialize() for food in Username]
+            Password_logs = [Password.serialize() for exercise in Password]
 
             return jsonify({'Username': Username_logs, 'Password': Password_logs}), 200
 
